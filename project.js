bfc64913bd7d80c6155c23542991a6ee9bbad471
nmukhin2251 <nick.mukhin@gmail.com>
2017-04-28 23:42:35 -0500

    all columns

---
+++ project.js
@@ -0,0 +1,1 @@
+define([], function( ) { return  JSON.stringify({"localVersion":1493439240042,"serverVersion":0,"components":[{"Title":"viewmodel","ContentTypeId":"0x010044953CC6D35CEF45B19CCF2AEC7E32780100AE6AAB5FC77750469531F47E7B19864A","Require":{"name":"viewmodel","proto":false,"path":"viewmodel"},"Markup":{"tag":"component-viewmodel"},"Params":{}}],"columns":[]});});
